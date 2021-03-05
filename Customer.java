a7f653ffc7f766b26ad35e4838ed30b66db0abd4
Filipe Vitorino <filipe0vit@gmail.com>
2021-03-05 10:57:29 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -19,13 +19,17 @@ public class Customer {
    }
 
    public String statement() {
+      
       double totalAmount = 0;
       int frequentRenterPoints = 0;
+      
       Enumeration rentals = _rentals.elements();
       String result = "Rental Record for " + getName() + "\n";
+      
       while (rentals.hasMoreElements()) {
+         
          Rental aRental = (Rental) rentals.nextElement();
-         double thisAmount = amountFor(aRental);
+         double thisAmount = aRental.getCharge();
          // add frequent renter points
          frequentRenterPoints ++;
          // add bonus for a two day new release rental
@@ -43,28 +47,5 @@ public class Customer {
       result += "You earned " + String.valueOf(frequentRenterPoints) +
                " frequent renter points";
       return result;
-   }
-
-
-   private double amountFor(Rental aRental) {
-      //Adicionar o trecho de código extraído.
-      //determine amounts for each line
-      double thisAmount = 0;
-      switch (aRental.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (aRental.getDaysRented() > 2)
-               thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += aRental.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (aRental.getDaysRented() > 3)
-               thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-            break;
-      }
-      return thisAmount;
    }
 }
