09470b85697a0d70a18d99722a682be55b11824a
Filipe Vitorino <filipe0vit@gmail.com>
2021-03-05 12:45:47 -0300

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -6,12 +6,12 @@ public class TextStatement extends Statement {
     
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
+        String result = " " + aCustomer.getName() +
         "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
+            result += each.getMovie().getTitle()+ " : " +
             String.valueOf(each.getCharge()) + "\n";
         }
         //add footer lines
