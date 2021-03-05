9bcd97f45ad04d5970415bf25bbf4c326c09d748
Filipe Vitorino <filipe0vit@gmail.com>
2021-03-05 12:48:44 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -2,23 +2,5 @@ import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
     
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = " " + aCustomer.getName() +
-        "\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            //show figures for this rental
-            result += each.getMovie().getTitle()+ " : " +
-            String.valueOf(each.getCharge()) + "\n";
-        }
-        //add footer lines
-        result += "Amount owed is " +
-        String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        " frequent renter points";
-        return result;
-    }
 
 }
--- Statement.java
+++ Statement.java
@@ -1,3 +1,23 @@
+import java.util.Enumeration;
+
 public class Statement {
-    
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = " " + aCustomer.getName() +
+        "\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for this rental
+            result += each.getMovie().getTitle()+ " : " +
+            String.valueOf(each.getCharge()) + "\n";
+        }
+        //add footer lines
+        result += "Amount owed is " +
+        String.valueOf(aCustomer.getTotalCharge()) + "\n";
+        result += "You earned " +
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        " frequent renter points";
+        return result;
+    }
 }
